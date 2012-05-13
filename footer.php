278fea9a1a161dda418b15e72f86a64dc0e0dcdc
Kenny Jackelen <kenny@kennyjackelen.com>
2012-05-13 16:38:16 -0500

    added necessary pieces for build script

--- footer.php
+++ footer.php
@@ -9,16 +9,19 @@
         </div><!-- #colophon -->
     </div><!-- #footer -->
 
+	</div><!-- #wrapper -->
+
   <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="<?php bloginfo('template_url'); ?>/js/libs/jquery-1.7.1.min.js"><\/script>')</script>
+
+  <!-- scripts concatenated and minified via build script -->
 	<script src="<?php bloginfo('template_url'); ?>/js/libs/bootstrap-transition.js"></script>
 	<script src="<?php bloginfo('template_url'); ?>/js/libs/bootstrap-dropdown.js"></script>
 	<script src="<?php bloginfo('template_url'); ?>/js/libs/bootstrap-collapse.js"></script>
 	<script src="<?php bloginfo('template_url'); ?>/js/libs/modernizr.js"></script>
 	<script src="<?php bloginfo('template_url'); ?>/js/slideshow.js"></script>
 	<script src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
-
-</div><!-- #wrapper -->
+  <!-- end scripts -->
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,4 +1,10 @@
 <!doctype html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!-- Consider adding a manifest.appcache: h5bp.com/d/Offline -->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -17,7 +23,7 @@
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 
-	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/style.css" />
 	
 	<?php wp_head(); ?>
 
